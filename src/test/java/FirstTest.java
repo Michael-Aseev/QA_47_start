a527e99b8efa6a74f930f63a1e45aed139425d67
michael <michaelaseev27@gmail.com>
2025-05-18 20:52:04 +0300

    start Css selection

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class FirstTest {
-    @Test
-    public void firstTest(){
+import java.time.Duration;
 
+public class FirstTest {
+    WebDriver driver = new ChromeDriver();
+
+
+    @Test
+    public void firstTest() {
+        // System.out.println("=================");
+        driver.get("https://ilcarro.web.app/search");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //=======================================
+        pause(3);
+        WebElement btnLetCarWork = driver.findElement(By.cssSelector("a[id='1']"));
+        btnLetCarWork.click();
+        pause(3);
+        WebElement btnTremOfUse = driver.findElement(By.cssSelector("*[ng-reflect-router-link='terms-of-use']"));
+        btnTremOfUse.click();
+        pause(3);
+        WebElement btnSighUp = driver.findElement(By.cssSelector("a[href='/registration?url=%2Fterms-of-use']"));
+        btnSighUp.click();
+        System.out.println(btnSighUp.getText());
+        pause(5);
+        WebElement inputName = driver.findElement(By.id("name"));
+        // WebElement inputName = driver.findElement(By.cssSelector("*[id='name']"));
+        inputName.sendKeys("Bob");
+        pause(2);
+        WebElement inputLastName = driver.findElement(By.cssSelector("input[autocomplete='family-name']"));
+        inputLastName.sendKeys("Dilan");
+
+
+    }
+
+    public static void pause(int time) {
+        try {
+            Thread.sleep(time * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
